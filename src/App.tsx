5ba6e2e2d2b183bcbd1663feb3d55c65e233470d
Albert <johanein@gmail.com>
2021-04-10 23:56:42 +0530

    end of second lesson successfully working

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-// import React from 'react'
+import './styles.css'
+import image from "./normal.png";
+import logo from "./logo.svg";
 
 const App = () => {
     return (
+        <>
+        <img src={image} alt="Normal size" width={300} height={200} />
+        <img src={logo} alt="svg logo" width={300} height={200} />
         <h1>
             React Typescript Webpack Starter Template
         </h1>
+        </>
     )
 }
 
